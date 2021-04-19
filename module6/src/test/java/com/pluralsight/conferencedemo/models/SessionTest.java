30d7805958a3957e178e3a8f0102abaaae69124e
ye Li <yeli.88@gmail.com>
2021-04-19 11:32:21 -0400

    completed Module 6

---
+++ module6/src/test/java/com/pluralsight/conferencedemo/models/SessionTest.java
@@ -0,0 +1,61 @@
+package com.pluralsight.conferencedemo.models;
+
+import com.pluralsight.conferencedemo.repositories.SessionJpaRepository;
+import com.pluralsight.conferencedemo.repositories.SessionRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class SessionTest {
+    @Autowired
+    private SessionRepository repository;
+
+    @Autowired
+    private SessionJpaRepository jpaRepository;
+
+    @Test
+    public void testCustomImpl() throws Exception {
+        List<Session> sessions = jpaRepository.customGetSessions();
+        assertTrue(sessions.size() > 0);
+    }
+
+    @Test
+    public void testPagingSorting() throws Exception {
+        Page<Session> page = jpaRepository.getSessionsWithName("S", PageRequest.of(1,5, Sort.by(Sort.Direction.DESC, "sessionLength")));
+        assertTrue(page.getTotalElements() > 0);
+    }
+
+    @Test
+    public void test() throws Exception {
+        List<Session> sessions = repository.getSessionsThatHaveName("Java");
+        assertTrue(sessions.size() > 0);
+    }
+
+    @Test
+    public void testNot() throws Exception {
+        List<Session> sessions = jpaRepository.findBySessionLengthNot(30);
+        assertTrue(sessions.size() > 0);
+    }
+
+    @Test
+    public void testNotLike() throws Exception {
+        List<Session> sessions = jpaRepository.findBySessionNameNotLike("Java%");
+        assertTrue(sessions.size() > 0);
+    }
+
+    @Test
+    public void testLessThan() throws Exception {
+        List<Session> sessions = jpaRepository.findBySessionLengthLessThan(45);
+        assertTrue(sessions.size() > 0);
+    }
+
+}
